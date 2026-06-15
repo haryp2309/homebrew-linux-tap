5ec070dea9788a1bb3987c400b8fa9c9278862b4
Hary Pirajan Mahalingam <hary2309@gmail.com>
2026-06-15 12:46:35 +0200

    feat: initial commit

---
+++ Formula/zen-browser.rb
@@ -0,0 +1,74 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://docs.brew.sh/rubydoc/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class ZenBrowser < Formula
+  desc "Welcome to a calmer internet"
+  homepage "https://zen-browser.app"
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
+  version_str = "1.21.1b"
+  main_sha256 = "a7facf3f12fe604f0bc5af3d93d8a5ef7325a3c6007fdcfc2aae6aa5e8100ed9"
+  metadata_sha256 = "2625bc819eb3942b1c8462cdf0c9c8cfced62041093e8d2a1e9d793825c27836"
+  url "https://github.com/zen-browser/desktop/releases/download/#{version_str}/zen.linux-#{arch}.tar.xz"
+  sha256 main_sha256
+  license "MPL-2.0"
+
+
+  depends_on :linux
+
+  livecheck do
+    url "https://github.com/zen-browser/desktop/releases"
+    strategy :github_latest
+  end
+
+  resource "metadata" do
+    url "https://github.com/zen-browser/flatpak/releases/download/#{version_str}/archive.tar"
+    sha256 metadata_sha256
+  end
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
+    # system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"zen"
+
+    resource("metadata").stage do
+      # The Flatpak App ID used as the filename
+      app_id = "app.zen_browser.zen"
+
+      # Install the official .desktop file directly into share/applications
+      (share/"applications").install "#{app_id}.desktop"
+
+      # Install the scalable SVG icon into the standard Linux hicolor theme directory
+      (share/"icons/hicolor/scalable/apps").install "icons/#{app_id}.svg"
+
+      # Install the AppStream metainfo file (helps software centers like GNOME Software recognize it)
+      #(share/"metainfo").install "#{app_id}.metainfo.xml"
+    end
+
+    inreplace share/"applications/app.zen_browser.zen.desktop" do |s|
+      # 1. Direct the shortcut to explicitly launch our Homebrew binary path
+      s.gsub!(/^Exec=.*$/, "Exec=#{bin}/zen %u")
+
+      # 2. CRITICAL: Turn off DBus activation so GNOME stops launching the Flatpak
+      #s.gsub!(/^DBusActivatable=true$/, "DBusActivatable=false")
+
+      # 3. OPTIONAL: Appends "(Homebrew)" to the app grid name so you can verify it works
+      s.gsub!(/^Name=Zen Browser$/, "Name=Zen Browser (Homebrew)")
+    end
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test zen-browser`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# Haryp2309 Linux-tap
+
+## How do I install these formulae?
+
+`brew install haryp2309/linux-tap/<formula>`
+
+Or `brew tap haryp2309/linux-tap` and then `brew install <formula>`.
+
+Or, in a `brew bundle` `Brewfile`:
+
+```ruby
+tap "haryp2309/linux-tap"
+brew "<formula>"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
